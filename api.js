27ee4fee65c0fd348eb96dbea6045eb8adbe932d
udav-py <93492256+udav-py@users.noreply.github.com>
2023-06-08 17:37:02 +0300

    first

---
+++ api.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+let a = 0;
+const apiKey = 'ade4109353483560bc671b9c447f34a6'; 
+
+const getWeatherData = async (city) => {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`;
+    const response = await axios.get(url);
+    const weatherData = response.data;
+    console.log(weatherData.name +" ",Math.round(weatherData.main.temp - 273.15));
+  } catch (error) {
+    console.error('Помилка отримання даних про погоду:', error);
+  }
+  
+};
+
+let cities = [703448,702658,709717]
+console.log("City", "Temperature")
+cities.forEach(x=>getWeatherData(x));
+
+console.log("https://api.openweathermap.org\nSachok Vladyslav Vitaliovich")
+
